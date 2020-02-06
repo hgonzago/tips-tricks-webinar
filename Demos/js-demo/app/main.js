f57e25dab1a3fab27611148e385fc5a013d59a0c
Kelly Hutchins <khutchins@esri.com>
2020-02-06 08:34:07 -0800

    update demo

--- Demos/js-demo/app/main.js
+++ Demos/js-demo/app/main.js
@@ -1,35 +1,40 @@
 require([
-  "esri/Map",
+  "esri/WebMap",
   "esri/views/MapView",
-  "esri/layers/Layer",
   "esri/widgets/Search",
   "esri/widgets/Search/LayerSearchSource",
+  "esri/Graphic",
+  "esri/layers/GraphicsLayer",
+  "esri/symbols/PictureMarkerSymbol",
   "dojo/domReady!"
 ], function (
-  Map,
   MapView,
-  LayerSearchSource,
+  WebMap,
   Search,
-  Layer
+  LayerSearchSource,
+  Graphic,
+  GraphicsLayer,
+  PictureMarkerSymbol
 ) {
-
-  const map = new Map({
-    basemap: "dark-gray-vector"
+  // Module Order: Webmap and Map view are flipped 
+  const map = new WebMap({
+    portalItem: {
+      id: "26d53be3e3f14f559c729d40a04f4b35"
+    }
   });
+
+
+  const graphicsLayer = new GraphicsLayer();
+  map.add(graphicsLayer);
 
   const view = new MapView({
     map: map,
-    zoom: 2,
     container: "viewDiv"
   });
 
-  // Module order: The Layer and LayerSearchSource modules 
-  // are flipped so this one will break 
-  Layer.fromPortalItem({
-    id: "6a57bf90b8cd46b7a5da9a3801f32163"
-  }).then(function (layer) {
-
-    map.add(layer);
+  // Make sure that the view/map is fully available in order to get the layer from the map
+  view.when(function () {
+    const layer = view.map.findLayerById("NGA_ASAM_1246");
     // TYPO: Mispell the class namefor layerSearchSource 
     // NETWORK: display field points to field with no info
     // inspect the network request to figure this out and 
@@ -51,10 +56,31 @@ require([
     const search = new Search({
       view: view,
       sources: [searchSource],
-      includeDefaultSources: false
+      includeDefaultSources: false,
+      goToOverride: function (view, goToParams) {
+        goToParams.target.zoom = 12;
+        return view.goTo(goToParams.target, goToParams.options);
+      }
+    });
+    view.ui.add(search, "top-trailing");
+
+    search.on("search-clear", function () {
+      graphicsLayer.removeAll();
     });
 
-    view.ui.add(search, "top-right");
+    // SECURITY: use a symbol with http 
+    const pms = new PictureMarkerSymbol({
+      url: "http://hgonzago.esri.com/sites/webinar/js-demo2copy/resources/pirate.png",
+      height: 20,
+      width: 20
+    });
+
+    search.on("select-result", function (event) {
+      if (event.result) {
+        const graphic = new Graphic(event.result.feature.geometry, pms)
+        graphicsLayer.add(graphic);
+      }
+    });
 
   });
 });
